c76a2de00f66891570ebb2667680ac6ca5afbf25
Pratik <pendurkarpratik04@gmail.com>
2023-10-12 23:47:26 +0530

    setting up api for shopping list

--- CookIT-backend/controller/ingredientController.js
+++ CookIT-backend/controller/ingredientController.js
@@ -18,6 +18,51 @@ const addIngredient = catchAsync(async (req, res, next) => {
   });
 });
 
+const getAllIngredients = catchAsync(async (req, res, next) => {
+  const ingredients = await Ingredient.find();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      ingredients,
+    },
+  });
+});
+
+// Get specific recipe's ingredients based on provided recipeId
+const getRecipeIngredients = catchAsync(async (req, res, next) => {
+  const recipe = await Ingredient.find({
+    recipeId: req.params.recipeId,
+  }).select('ingredientName quantity -_id');
+  res.status(200).json({
+    status: 'success',
+    data: {
+      recipe,
+    },
+  });
+});
+
+// Get all unique recipe names and IDs
+const getUniqueRecipes = catchAsync(async (req, res, next) => {
+  const recipes = await Ingredient.aggregate([
+    {
+      $group: {
+        _id: { recipeId: '$recipeId', recipeName: '$recipeName' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        recipeId: '$_id.recipeId',
+        recipeName: '$_id.recipeName',
+      },
+    },
+  ]);
+  res.status(200).json(recipes);
+});
+
 module.exports = {
   addIngredient,
+  getRecipeIngredients,
+  getAllIngredients,
+  getUniqueRecipes,
 };
--- CookIT-backend/routes/IngredientRouter.js
+++ CookIT-backend/routes/IngredientRouter.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.route('/').post(ingredientController.addIngredient);
 
+router.route('/').get(ingredientController.getAllIngredients);
+
+router
+  .route('/recipe/:recipeId')
+  .get(ingredientController.getRecipeIngredients);
+
+router.route('/getUniqueRecipes').get(ingredientController.getUniqueRecipes);
+
 module.exports = router;
